07892bfc6058c31c647a081dfe7b4a6f118c04da
wangheng <wang.heng@asiainfo-sec.com>
2022-05-11 09:23:42 +0800

    fix: redis

--- src/modules/cache/cache.module.ts
+++ src/modules/cache/cache.module.ts
@@ -3,11 +3,13 @@ import { CacheService } from './cache.service';
 import { RedisModule } from 'nestjs-redis';
 import { ConfigService } from '@nestjs/config';
 
-
 @Module({
-  imports: [RedisModule.forRootAsync({
-    useFactory: (configService: ConfigService) => configService.get('redis');
-  })],
+  imports: [
+    RedisModule.forRootAsync({
+      useFactory: (configService: ConfigService) => configService.get('redis'),
+      inject: [ConfigService],
+    }),
+  ],
   // controllers: [CacheController],
   providers: [CacheService],
   exports: [CacheService],
