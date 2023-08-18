c8d2576e4f9a01e8826aae665d1c7e881f475cb3
ALLEWAN3 <allen.al.wang@dbschenker.com>
2023-08-18 20:43:20 +0800

    feat: test

--- src/modules/resource/resource.service.spec.ts
+++ src/modules/resource/resource.service.spec.ts
@@ -1,18 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ResourceService } from './resource.service';
+import { CreateResourceDto } from './dto/create-resource.dto';
+import { CategoryService } from '../category/category.service';
 
 describe('ResourceService', () => {
   let service: ResourceService;
+  let categoryService: CategoryService
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ResourceService],
+      providers: [ResourceService, CategoryService],
     }).compile();
 
     service = module.get<ResourceService>(ResourceService);
+    categoryService = module.get<CategoryService>(CategoryService);
+    // catsController = moduleRef.get<Repository>(CatsController);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('test created', () => {
+    const params: CreateResourceDto = {
+      name: 'test resource',
+      url: 'baidu.com',
+      haveCode: true,
+      code: '',
+      isPublic: false,
+      score: 100,
+      categoryId: 'categoryId',
+    };
+    const userId = 'userId';
+    jest.spyOn(categoryService, 'findById').mockImplementation(() => ({}))
+    const result = service.create(params, userId);
+    // expect(service).toBeDefined();
   });
 });
