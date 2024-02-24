3edbb84a4b7ad2dc61a2bc9056c9ce5976a32074
wangheng3 <wangheng3@360.cn>
2024-02-24 22:01:06 +0800

    fix:

--- config/configuration.ts
+++ config/configuration.ts
@@ -6,6 +6,8 @@ const YAML_CONFIG_FILENAME =
   process.env.NODE_ENV === 'production'
     ? 'config.prod.yaml'
     : 'config.dev.yaml';
+console.log('config:', process.env.NODE_ENV);
+// console.log('config:', process.env);
 export default () => {
   return yaml.load(
     readFileSync(join(__dirname, YAML_CONFIG_FILENAME), 'utf8'),
