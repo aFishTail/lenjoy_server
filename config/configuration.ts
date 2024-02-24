56bc2c240c67c329ebda121fa1e9e088f8b49968
wangheng3 <wangheng3@360.cn>
2024-02-24 21:37:17 +0800

    fix:

--- config/configuration.ts
+++ config/configuration.ts
@@ -2,11 +2,13 @@ import { readFileSync } from 'fs';
 import * as yaml from 'js-yaml';
 import { join } from 'path';
 
+//TODO：环境变量获取不到
 const YAML_CONFIG_FILENAME =
   process.env.NODE_ENV === 'production'
     ? 'config.prod.yaml'
     : 'config.dev.yaml';
-
+console.log('config:', process.env.NODE_ENV);
+console.log('config:', process.env);
 export default () => {
   return yaml.load(
     readFileSync(join(__dirname, YAML_CONFIG_FILENAME), 'utf8'),
